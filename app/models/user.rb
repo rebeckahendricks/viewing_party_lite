1e3b350905edb052d36de84fe203488b90da277a
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-10-24 15:57:05 -0600

    Update validations : password_digest versus password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
-  
+  validates :password_digest, presence: true
+
   has_secure_password
 end
